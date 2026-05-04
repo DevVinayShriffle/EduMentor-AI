4d91f7d6dab6e7fcf2136b5a30d241746af7de8b
aadi-insane <adityaaerpule@rediffmail.com>
2026-05-04 16:03:39 +0530

    Added user session and registration controller and also devise routes

---
+++ backend/app/controllers/users/registrations_controller.rb
@@ -0,0 +1,31 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  respond_to :json
+
+  private
+
+  def respond_with(resource, _opts = {})
+    if resource.persisted?
+      render json: {
+        message: 'Signed up successfully',
+        user: {
+          id: resource.id,
+          email: resource.email,
+          role: resource.role
+        }
+      }, status: :created
+    else
+      render json: {
+        message: 'Signup failed',
+        errors: resource.errors.full_messages
+      }, status: :unprocessable_entity
+    end
+  end
+
+  def sign_up_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :role)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :current_password)
+  end
+end
